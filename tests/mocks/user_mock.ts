8d08d7bba98f2d99bf669a0887bd15f16686d5fe
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-05-10 15:16:11 -0400

    refactor: move user input to a mock funtion

---
+++ tests/mocks/user_mock.ts
@@ -0,0 +1,14 @@
+import { User } from "@/domain/models"
+
+export const UserMock = (): User => {
+  return {
+    id: "id",
+    user_name: "Jhon Doe",
+    loans: [
+      {
+        date: new Date(2017,2,10),
+        status: "paid"
+      }
+    ]
+  }
+}
