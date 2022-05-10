4c81a88bd9bdc441dc4eca26943a4bb0aa2e3ee8
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-05-10 15:31:06 -0400

    refactor: rename entity files with sufix entity

---
+++ src/domain/entities/create_loan_request_entity.ts
@@ -0,0 +1,27 @@
+import {CreateLoanRequestModel} from "../models"
+
+export class CreateLoanRequestEntity{
+  public async request(
+    input: CreateLoanRequestModel.Input
+  ): Promise<CreateLoanRequestModel.Output> {
+
+    if (input.loan > 1000000)
+      throw new Error(
+        "Loan not permited, please choice a value lower than 1000000"
+      )
+
+    input.user.loans.map(loan => {
+      if (loan.status === "unpaid")
+        throw new Error(
+          "Impossible request a new Loan, you have loan unpaid"
+        )
+    })
+
+    const user_id = input.user.id
+
+    return {
+      ...input,
+      user_id
+    }
+  }
+}
