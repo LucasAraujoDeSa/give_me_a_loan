df0c133714953fd881f0618718da0a4d25aaa2d9
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-05-17 16:32:14 -0400

    test: make loan_request test

--- tests/create_loan_request.spec.ts
+++ tests/create_loan_request.spec.ts
@@ -1,65 +1,55 @@
-import { CreateLoanRequest } from "@/domain/entities/create_loan_request"
-import { UserMock, LoanRequestMock } from "./mocks"
+import { CreateLoanRequest } from "@/application/use_cases/create_loan_request"
+import {
+  SaveLoanRequestDatabaseInMemory,
+  GetUserByIdDatabaseInMemory
+} from "./database_in_memory"
 
 const makeSut = () => {
-  const sut = new CreateLoanRequest()
-
+  const saveLoanRequest = new SaveLoanRequestDatabaseInMemory()
+  const getUserById = new GetUserByIdDatabaseInMemory()
+  const sut = new CreateLoanRequest(
+    getUserById,
+    saveLoanRequest
+  )
   return {
-    sut
+    sut,
+    getUserById,
+    saveLoanRequest
   }
 }
-
-
-describe("==> create loan request", () => {
-  it("should create loan request", async () => {
+describe('==> create loan request', () => {
+  it('should request a loan', async () => {
     const { sut } = makeSut()
 
     // inputs
-    const user = UserMock()
-    const input = LoanRequestMock(user)
+    const input = {
+      loan: 1000,
+      user_id: "id"
+    }
 
-    const test = await sut.request(input);
+    const testSpy = jest.spyOn(sut, "execute")
+    await sut.execute(input)
 
-    expect(test.user_id).toBe(user.id);
-    expect(test.loan).toBe(input.loan);
+    expect(testSpy).toHaveBeenCalled()
   });
 
-  it("should throw a erro if loan is high than 1000000", async () => {
-    const { sut } = makeSut()
+  it('should save a request at database with status on going', async () => {
+    const { sut, saveLoanRequest } = makeSut()
 
     // inputs
-    const user = UserMock()
-    const input = LoanRequestMock(user)
+    const input = {
+      loan: 1000,
+      user_id: "id"
+    }
 
-    expect(
-      sut.request({
-        ...input,
-        loan: 10000000
-      })
-    ).rejects.toBeInstanceOf(Error);
-  });
+    const testSpy = jest.spyOn(saveLoanRequest, "save")
+    await sut.execute(input)
 
-  it("should throw a erro if user has a loan with status unpaid", async () => {
-    const { sut } = makeSut()
-
-    // inputs
-    const user = UserMock()
-    const input = LoanRequestMock(user)
-
-    expect(
-      sut.request({
-        ...input,
-        user: {
-          ...user,
-          loans:
-          [
-            {
-              date: new Date(Date.now()),
-              status: "unpaid"
-            }
-          ]
-        },
-      })
-    ).rejects.toBeInstanceOf(Error);
+    expect(testSpy).toHaveBeenCalledWith({
+      status: "on going",
+      loan: input.loan,
+      user_id: input.user_id,
+      date: saveLoanRequest.date
+    })
   });
 });
