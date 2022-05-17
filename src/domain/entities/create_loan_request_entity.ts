feb2bbb30eb19e5c3a768f24c01d55ef6371fbf5
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-05-17 18:01:36 -0400

    refactor: change error exception class

--- src/domain/entities/create_loan_request_entity.ts
+++ src/domain/entities/create_loan_request_entity.ts
@@ -1,3 +1,4 @@
+import { ErrorHandler } from "@/core/error/error_handler"
 import {CreateLoanRequestModel} from "../models"
 
 export class CreateLoanRequestEntity{
@@ -6,14 +7,18 @@ export class CreateLoanRequestEntity{
   ): Promise<CreateLoanRequestModel.Output> {
 
     if (input.loan > 1000000)
-      throw new Error(
-        "Loan not permited, please choice a value lower than 1000000"
+      throw new ErrorHandler(
+        400,
+        "Loan not permited, please choice a value lower than 1000000",
+        "BadRequest"
       )
 
     input.user.loans.map(loan => {
       if (loan.status === "unpaid")
-        throw new Error(
-          "Impossible request a new Loan, you have loan unpaid"
+        throw new ErrorHandler(
+          400,
+          "Impossible request a new Loan, you have loan unpaid",
+          "BadRequest"
         )
     })
 
